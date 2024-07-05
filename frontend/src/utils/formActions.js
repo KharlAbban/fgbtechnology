840b5e938c7175611855d16d3810592318ed112c
KharlAbban <khvngkharl123@gmail.com>
2024-07-05 11:34:03 +0000

    Change API url in form action

--- frontend/src/utils/formActions.js
+++ frontend/src/utils/formActions.js
@@ -4,10 +4,9 @@ export async function homePageAction ({request}) {
     const formData = await request.formData();
     const emailContents = Object.fromEntries(formData);
     
-    console.log(emailContents.message)
 
     try {
-        const response = await axios.post("http://localhost:3000/email", emailContents);
+        const response = await axios.post("https://fgbtechnology-api.vercel.app/", emailContents);
 
         console.log(response.data);
         alert("Email sent successfully");
